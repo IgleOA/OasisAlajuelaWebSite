723644e67a8068ce06c51b0cff3b558c79ee63da
johmstone <61640703+johmstone@users.noreply.github.com>
2020-08-26 23:04:32 -0600

    Changes 08/26/2020

--- OasisAlajuelaAPI/Controllers/SermonsController.cs
+++ OasisAlajuelaAPI/Controllers/SermonsController.cs
@@ -6,6 +6,9 @@ using System.Net.Http;
 using System.Web.Http;
 using ET;
 using BL;
+using System.Web.Http.Description;
+using OasisAlajuelaAPI.Filters;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace OasisAlajuelaAPI.Controllers
 {
@@ -13,16 +16,93 @@ namespace OasisAlajuelaAPI.Controllers
     {
         private SermonsBL SBL = new SermonsBL();
 
-        [HttpGet]
-        public IEnumerable<Sermons> List()
+        [HttpPost]
+        [ResponseType(typeof(List<Sermons>))]
+        public HttpResponseMessage List()
         {
-            return SBL.List(false);
+            var r = SBL.List(true);
+
+            return this.Request.CreateResponse(HttpStatusCode.OK, r);
         }
 
-        [HttpGet]
-        public Sermons Details(int id)
+        [HttpPost]
+        [ApiKeyAuthentication]
+        [Route("api/Sermons/History")]
+        [ResponseType(typeof(List<Sermons>))]
+        public HttpResponseMessage History()
         {
-            return SBL.Details(id);
+            var r = SBL.List(false);
+
+            return this.Request.CreateResponse(HttpStatusCode.OK, r);
+        }
+
+        [HttpPost]
+        [ApiKeyAuthentication]
+        [ResponseType(typeof(Sermons))]
+        public HttpResponseMessage Details(int id)
+        {
+            var r = SBL.Details(id);
+
+            if (r.SermonID > 0)
+            {
+                return this.Request.CreateResponse(HttpStatusCode.OK, r);
+            }
+            else
+            {
+                return this.Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+        }
+
+        [HttpPost]
+        [ApiKeyAuthentication]
+        [Route("api/Sermons/Update")]
+        [ResponseType(typeof(bool))]
+        public HttpResponseMessage Update([FromBody] Sermons model)
+        {
+            var authHeader = this.Request.Headers.GetValues("Authorization").FirstOrDefault();
+            var token = authHeader.Substring("Bearer ".Length);
+            var handler = new JwtSecurityTokenHandler();
+            var jsonToken = handler.ReadToken(token);
+            var tokenS = handler.ReadToken(token) as JwtSecurityToken;
+
+            var UserName = tokenS.Claims.First(claim => claim.Type == "UserName").Value;
+
+            var r = SBL.Update(model, UserName);
+
+            if (!r)
+            {
+                return this.Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+            else
+            {
+                return this.Request.CreateResponse(HttpStatusCode.OK, r);
+            }
+        }
+
+        [HttpPost]
+        [ApiKeyAuthentication]
+        [Route("api/Sermons/AddNew")]
+        [ResponseType(typeof(bool))]
+        public HttpResponseMessage AddNew([FromBody] Sermons model)
+        {
+            var authHeader = this.Request.Headers.GetValues("Authorization").FirstOrDefault();
+            var token = authHeader.Substring("Bearer ".Length);
+            var handler = new JwtSecurityTokenHandler();
+            var jsonToken = handler.ReadToken(token);
+            var tokenS = handler.ReadToken(token) as JwtSecurityToken;
+
+            var UserName = tokenS.Claims.First(claim => claim.Type == "UserName").Value;
+
+            var r = SBL.AddNew(model, UserName);
+
+            if (r > 0)
+            {
+                return this.Request.CreateResponse(HttpStatusCode.OK, true);                
+            }
+            else
+            {
+                return this.Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
         }
     }
 }
